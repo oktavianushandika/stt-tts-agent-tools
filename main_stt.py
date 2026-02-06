f603e00280874fa9d70f4464da3eab4b717d1a24
Oktavianus Handika <oktavianus.handika@gdplabs.id>
2026-02-06 17:42:25 +0700

    Reverted stt agent instruction & updated poetry lock

--- main_stt.py
+++ main_stt.py
@@ -20,13 +20,6 @@ def main():
         Use the available tools to help users with speech to text conversion.
         When the user provides an audio file, use the stt tool to convert it directly to base64 format first,
         then use the stt tool to convert the base64 format to text.
-        If you have trouble accessing the audio file, immediately use local path provided or try fetch it from the provided s3 URL if there is any.
-        The audio file is provided in the files parameter.
-        Use the direct local path of the audio file in /tmp/agent_files_<random_string>/<random_string>/<audio_file> for the audio file.
-        If the audio is uploaded to s3 bucket, use the s3 path for the audio file.
-        Example:
-        /tmp/agent_files_<random_string>/<random_string>/<audio_file>
-        s3://<bucket_name>/<audio_file>
         The audio file is provided in the files parameter.""",
         tools=[STTTool],
     )
